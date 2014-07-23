3201146b369fcbee0c77a505337e7809edecb2fe
misoobu <misoobu@me.com>
2014-07-23 10:11:43 +0900

    Add initialization NeoBundleInstall

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
-#!/bin/sh
+#!/bin/bash
+
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.zsh_profile ~/.zsh_profile
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
+
+[ ! -d ~/.vim/bundle ] && mkdir -p ~/.vim/bundle && git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim && vim -c ':NeoBundleInstall'
+
