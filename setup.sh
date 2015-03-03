60abffe0eafa5ea57d3163c7a46a12206ea68fbf
misoobu <misoobu@me.com>
2015-03-03 13:46:12 +0900

    Add .tigrc

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.zsh_profile ~/.zsh_profile
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.zshrc.alias ~/.zshrc.alias
+ln -sf ~/dotfiles/.tigrc ~/.tigrc
 
 [ ! -d ~/.vim/bundle ] && mkdir -p ~/.vim/bundle && git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim && vim -c ':NeoBundleInstall'
 
