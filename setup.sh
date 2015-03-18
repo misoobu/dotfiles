a9c9f5537452d157063116bc296c4acce4537eb9
misoobu <misoobu@me.com>
2015-03-18 15:56:25 +0900

    Add .zshenv

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.zsh_profile ~/.zsh_profile
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.zshenv ~/.zshenv
 ln -sf ~/dotfiles/.zshrc.alias ~/.zshrc.alias
 ln -sf ~/dotfiles/.tigrc ~/.tigrc
 
