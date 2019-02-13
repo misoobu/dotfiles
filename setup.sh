f8132d63a482ff0c3a9924339c56d9345951151f
misoobu <misoobu@me.com>
2019-02-13 18:40:00 +0900

    Remove unused file

--- setup.sh
+++ setup.sh
@@ -5,7 +5,6 @@
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/.zshenv ~/.zshenv
 ln -sf ~/dotfiles/.zshrc.alias ~/.zshrc.alias
 ln -sf ~/dotfiles/.tigrc ~/.tigrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
