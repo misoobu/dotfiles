83620bea92c114a00e56f59b6d776a3133b2d40c
misoobu <misoobu@me.com>
2020-05-31 11:39:36 +0900

    Cleanup setup.sh

--- setup.sh
+++ setup.sh
@@ -1,16 +1,16 @@
-#!/bin/bash
-
-# TODO: homebrew, homebrew-bundle
+#!/bin/sh -ex
 
 ln -sf ~/dotfiles/.alacritty.yml ~/.alacritty.yml
+mkdir -p ~/.bundle
+ln -sf ~/dotfiles/.bundle_config ~/.bundle/config
+ln -sf ~/dotfiles/.gemrc ~/.gemrc
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/.gitignore ~/.gitignore
+ln -sf ~/dotfiles/.tigrc ~/.tigrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.tmux.split.conf ~/.tmux.split.conf
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.zshrc.alias ~/.zshrc.alias
-ln -sf ~/dotfiles/.tigrc ~/.tigrc
-ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
-ln -sf ~/dotfiles/.gitignore ~/.gitignore
-ln -sf ~/dotfiles/.gemrc ~/.gemrc
-mkdir ~/.bundle
-ln -sf ~/dotfiles/.bundle_config ~/.bundle/config
+
+# You should install homebrew (https://brew.sh/), and run `brew bundle install`
