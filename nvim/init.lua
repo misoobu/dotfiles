cc1069be723a172a1e01e50e09b274753dcea55e
misoobu <misoobu@me.com>
2023-11-09 19:08:19 -0600

    nvim: Install colorscheme

--- nvim/init.lua
+++ nvim/init.lua
@@ -30,4 +30,21 @@ vim.api.nvim_create_autocmd("BufWritePre", {
   command = ":%s/\\s\\+$//e",
 })
 
--- TODO: plugins
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+})
+
+vim.cmd.colorscheme "catppuccin-mocha"
