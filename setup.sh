fdf90be15cfa1008d56ce0acd3ec4d6057e9182d
misoobu <misoobu@me.com>
2024-07-13 16:40:36 -0500

    don't want to overwrite something accidentally

--- setup.sh
+++ setup.sh
@@ -1,18 +1,18 @@
 #!/bin/sh -ex
 
-ln -sf ~/dotfiles/.alacritty.toml ~/.alacritty.toml
+ln -s ~/dotfiles/.alacritty.toml ~/.alacritty.toml
 mkdir -p ~/.bundle
-ln -sf ~/dotfiles/.bundle_config ~/.bundle/config
-ln -sf ~/dotfiles/.gemrc ~/.gemrc
-ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
-ln -sf ~/dotfiles/.tigrc ~/.tigrc
-ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -sf ~/dotfiles/.tmux.split.conf ~/.tmux.split.conf
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.zprofile ~/.zprofile
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/.zshrc.alias ~/.zshrc.alias
-ln -sf ~/dotfiles/nvim ~/.config/
-ln -sf ~/dotfiles/.fdignore ~/.fdignore
+ln -s ~/dotfiles/.bundle_config ~/.bundle/config
+ln -s ~/dotfiles/.gemrc ~/.gemrc
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/.tigrc ~/.tigrc
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/.tmux.split.conf ~/.tmux.split.conf
+ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.zprofile ~/.zprofile
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.zshrc.alias ~/.zshrc.alias
+ln -s ~/dotfiles/nvim ~/.config/
+ln -s ~/dotfiles/.fdignore ~/.fdignore
 
 # You should install homebrew (https://brew.sh/), and run `brew bundle install`
