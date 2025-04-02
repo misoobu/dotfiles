1fcff33b792595c4daddcdc4ecda47916c79df17
misoobu <misoobu@me.com>
2025-04-02 15:42:43 -0700

    update nvim plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -243,7 +243,7 @@ require("lazy").setup({
     "yetone/avante.nvim",
     event = "VeryLazy",
     lazy = false,
-    version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
+    version = false,
     opts = {
       file_selector = {
         provider = "telescope",
@@ -251,6 +251,7 @@ require("lazy").setup({
     },
     build = "make",
     dependencies = {
+      "nvim-treesitter/nvim-treesitter",
       "stevearc/dressing.nvim",
       "nvim-lua/plenary.nvim",
       "MunifTanjim/nui.nvim",
